78dea58dd2fbc6a796683e168722b935e4c7c41f
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-08 08:05:48 +0300

    Created book controller (2 Books/get method)

---
+++ AspNetSandbox2/BooksController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetSandbox2
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService booksService;
+
+
+        public BooksController( IBooksService booksService)
+        {
+            this.booksService = booksService;
+        }
+        
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return booksService.Get() ;
+        }
+        private bool SomeFunction(Book book)
+        {
+            return book.Id == 1;
+        }
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+         
+            return booksService.Get(id);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book value)
+        {
+            booksService.Post(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            booksService.Delete(id);
+        }
+    }
+}
