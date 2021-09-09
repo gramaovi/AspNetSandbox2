ce0538b3a7c8495a0af812a72ae006179298ca86
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-09 11:27:49 +0300

    Added migration and added Book Menu Item

--- DbSandbox/Data/ApplicationDbContext.cs
+++ DbSandbox/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DbSandbox.Models;
 
 namespace DbSandbox.Data
 {
@@ -12,5 +13,6 @@ namespace DbSandbox.Data
             : base(options)
         {
         }
+        public DbSet<DbSandbox.Models.Book> Book { get; set; }
     }
 }
--- DbSandbox/Data/Migrations/20210909081435_AddBooks.Designer.cs
+++ DbSandbox/Data/Migrations/20210909081435_AddBooks.Designer.cs
@@ -4,14 +4,16 @@ using DbSandbox.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DbSandbox.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210909081435_AddBooks")]
+    partial class AddBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
