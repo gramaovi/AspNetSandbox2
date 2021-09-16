02776795a8799f5af013820a834115490e0c0301
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-16 21:22:03 +0300

    Razer pages UI for controller

---
+++ AspNetSandbox2/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AspNetSandbox2.Data;
+using AspNetSandbox2.Models;
+
+namespace AspNetSandbox2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AspNetSandbox2.Data.ApplicationDbContext _context;
+
+        public DetailsModel(AspNetSandbox2.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ AspNetSandbox2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AspNetSandbox2.Data;
+using AspNetSandbox2.Models;
+
+namespace AspNetSandbox2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AspNetSandbox2.Data.ApplicationDbContext _context;
+
+        public IndexModel(AspNetSandbox2.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
