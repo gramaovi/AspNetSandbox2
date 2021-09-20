f4843776a7e8b95964a5f796e4a89a3feb21be14
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-20 04:20:35 +0300

    PurchaseMigrations added

--- AspNetSandbox2/Migrations/20210920011958_PurchaseAddedMigration.Designer.cs
+++ AspNetSandbox2/Migrations/20210920011958_PurchaseAddedMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using AspNetSandbox2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace AspNetSandbox2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210920011958_PurchaseAddedMigration")]
+    partial class PurchaseAddedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
