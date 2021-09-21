77038bf015c85504effb270eced110d446b5801d
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-22 00:05:22 +0300

    Added delete,create,update to books.js

--- AspNetSandbox2/wwwroot/js/books.js
+++ AspNetSandbox2/wwwroot/js/books.js
@@ -1,12 +1,57 @@
 ﻿"use strict";
-
 var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build();
 
 connection.start().then(function () {
-    console.log("Connection established.");
+    console.log("Conection workingd");
 }).catch(function (err) {
     return console.error(err.toString());
 });
+
+
+
 connection.on("BookCreated", function (book) {
-    console.log('Book Created: $(JSON.stringify(book)}');
+    console.log(`Book Created: ${JSON.stringify(book)}`);
+
+    $("tbody").append(`<tr>
+        <td>
+        ${book.title}
+        </td>
+        <td>
+        ${book.author}
+        </td>
+        <td>
+        ${book.language}
+        </td>
+        <td>
+        <a href="/Books/Edit?id=${book.id}">Edit</a> |
+        <a href="/Books/Details?id=${book.id}">Details</a> |
+        <a href="/Books/Delete?id=${book.id}">Delete</a>
+        </td>
+        </tr>`);
+});
+
+connection.on("BookUpdated", function (book) {
+    console.log(`Book Updated: ${JSON.stringify(book)}`);
+    var row = document.getElementById(`book-${book.id}`);
+    row.innerHTML = `
+        <td>
+            ${book.title}
+        </td>
+        <td>
+            ${book.author}
+        </td>
+        <td>
+            ${book.language}
+        </td>
+        <td>
+            <a href="/Books/Edit?id=${book.id}">Edit</a> |
+            <a href="/Books/Details?id=${book.id}">Details</a> |
+            <a href="/Books/Delete?id=${book.id}">Delete</a>
+        </td>`
+});
+
+connection.on("BookDeleted", function (book) {
+    console.log(`Book Deleted: ${JSON.stringify(book)}`);
+    var row = document.getElementById(`book-${book.id}`);
+    row.parentNode.removeChild(row);
 });
