9a772f455620c2df7f99a4e58b4f32be77c0e23d
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-22 12:37:58 +0300

    Created EnumerateFilesTest Method

---
+++ AspNetSandbox.Tests/FileOperationWithBooks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetSandbox.Tests
+{
+    public class FileOperationWithBooks
+    {
+        [Fact]
+        public void EnumerateFilesTest()
+        {
+            System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(".");
+            var files=directoryInfo.EnumerateFiles();
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}
